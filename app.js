b4df0ceed983e3d93c218782b97c985c987e435d
Kushal Shrestha <kushalshrestha02@gmail.com>
2021-09-06 17:24:23 +0545

    Adding logo and about us page

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+//active class //
+// const currentLoaction = location.href;
+// const menuItem = document.querySelectorAll("a");
+// const menuLength = menuItem.length
+
+// for (let i=0; i<menuLength; i++) {
+//     if (menuItem[i].href === currentLoaction) {
+//         menuItem[i].className = "active"
+//     }
+// }
+// const navActive = document.querySelector("navbar").querySelectorAll("a");
+
+// navActive.forEach(element => {
+//     element.addEventListener('click', function() {
+//         navActive.forEach(nav=>nav.classList.remove("active"))
+
+//         this.classList.add("active");
+//     })
+// })
+
+// burger menu //
 const burgerMenu = document.getElementsByClassName('burger-menu')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
 
