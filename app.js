044702a8bc730125c499828be3f037b2b3d86cd0
Kushal Shrestha <kushalshrestha02@gmail.com>
2021-09-12 19:12:58 +0545

    Addding carousel

--- app.js
+++ app.js
@@ -11,6 +11,34 @@ li.forEach(el => {
 });
 
 
+// Carosel 
+var carouselIndex = 1;
+carousel(carouselIndex);
+
+function plusCarousel(n) {
+    carousel(carouselIndex += n);
+}
+
+function currentCarousel (n) {
+    carousel(carouselIndex = n);
+}
+
+function carousel(n) {
+    var i;
+    var slides = document.getElementsByClassName("carousel");
+    var dot = document.getElementsByClassName("dot");
+    if (n > slides.length) {carouselIndex = 1}    
+    if (n < 1) {carouselIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dot.length; i++) {
+        dot[i].className = dot[i].className.replace(" activeCarousel", "");
+    }
+    slides[carouselIndex-1].style.display = "block";  
+    dot[carouselIndex-1].className += " activeCarousel";
+}
+
 // burger menu //
 const burgerMenu = document.getElementsByClassName('burger-menu')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
